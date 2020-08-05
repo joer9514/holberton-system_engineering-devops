00534281f35d60375b9072f242bf8f77b32ee10b
Jose Espinosa <joer9514@gmail.com>
2020-08-04 20:26:31 -0500

    task advance

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# ssh key configuration with Puppet
+
+file_line { 'ssh identity':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'ssh_key':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
+}
